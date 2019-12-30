f6cb1e85965a12b306c984caba428e74cf37dfcd
Zandew <andewzxue@gmail.com>
2019-12-30 17:16:21 -0500

    ride

--- Profile.h
+++ Profile.h
@@ -1,8 +1,12 @@
 class Profile{
 	protected:
 		string name;
+		double balance;
+		vector<Ride> rides;
 	public:
 		Profile(string name);
 		~Profile();
+		void addRide(Ride ride);
+		void payRide(int idx);
 };
 
