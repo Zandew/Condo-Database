49688e958b450675bd35552eec44868752476c9a
Zandew <andewzxue@gmail.com>
2019-12-31 19:25:22 -0500

    u

--- Ride.h
+++ Ride.h
@@ -1,1 +1,11 @@
+#include <iostream>
+#include "Driver.h"
 
+class Ride{
+	private:
+		Driver driver;
+		string source, destination;
+	public:
+		Ride(Driver driver, string source, string destination);
+		~Ride();
+}
