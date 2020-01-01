3a1bdb093b188e9b581599870c8ca9d24e610eaa
Zandew <andewzxue@gmail.com>
2019-12-31 19:08:56 -0500

    i

--- Profile.cpp
+++ Profile.cpp
@@ -1,1 +1,16 @@
+#include <Profile.h>
+#include "Ride.h"
+
+Profile::Profile(string name){
+	Profile::name = name;
+	Profile::balance = 0;
+}
+
+Profile::~Profile(){
+
+}
+
+void Profile::addRide(Ride ride){
+	Profile::rides.push_back(ride);
+}
 
